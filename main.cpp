e6b7af3665262ee9f4179844a145015eb52273b6
michelle-angelic <michelle.santiago19@upr.edu>
2025-02-14 12:59:28 +0000

    primera parte de la fracción

--- main.cpp
+++ main.cpp
@@ -8,6 +8,30 @@ using namespace std;
 
 int main(){
   // desplegar propósito del código 
+
+  cout << "Este programa suma dos fracciones."; 
+
+  // declarar variables para evitar problemas después
+
+  double num_1;
+  double denom_1;
+
+  double num_2;
+  double denom_2;
+
+  // pedir numerador de 1ra fracción
+
+  cout << "Entre el numerador de la primera fracción: ";
+  cin >> num_1;
+
+  // pedir denominador de 1ra fracción
+
+  cout << "Entre el denominador de la primera fracción: ";
+  
+  cin >> denom_1;
+
+
+
   
   
   return 0;
