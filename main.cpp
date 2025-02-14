637cc2a285764daaf7b200b416d814abf1f0646e
michelle-angelic <michelle.santiago19@upr.edu>
2025-02-14 12:55:27 +0000

    first changes + comments

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
-// escriba su nombre, username en github y numero de estudiante en el header
-// incluya cualquier colaboración que haga
+// Michelle A. Santiago, michelle-angelic 
+// 801 - 23 - 5483
+// Colaboraciones: 
+// Asignación: Suma de fracciones
+
 #include<iostream>
 using namespace std;
 
 int main(){
-  // escriba su código aquí 
+  // desplegar propósito del código 
+  
   
   return 0;
 }
